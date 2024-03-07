a23f44c7f624e2be0a3757a29b5c1275ee8599e8
Jack Holy <jholy@carroll.edu>
2024-03-06 21:28:23 -0700

    Add proper comments and headers

--- main.go
+++ main.go
@@ -1,3 +1,10 @@
+/*
+Author: Jack Holy
+Description: A Go program which allows the user to keep track of a scores.
+The user has the option to add new scores, look at the past scores, check
+the mean (average) of all the scores, and quit the program. Any whole number
+can be entered as a score, and strings are not valid.
+*/
 package main
 
 import (
@@ -7,24 +14,36 @@ import (
 	"strconv"
 )
 
+// Create an empty array of scores.
 var scores []int
 
+/*
+Allows the user to enter a new score. Allows type int as valid input,
+and adds it to the array.
+@return string
+*/
 func newScore() string {
 	for {
 		fmt.Println("Enter a new score: ")
 		var score int
+		// Get input from the user
 		_, err := fmt.Scan(&score)
+		// Catch errors that are passed into the array of scores.
 		if err != nil {
 			fmt.Println("\nInvalid input. Please enter an integer.")
 		} else {
 			scores = append(scores, score)
-			// sort the list of scores
+			// Sort the array of scores
 			sort.Ints(scores)
 			return "Added " + strconv.Itoa(score) + " to list of scores."
 		}
 	}
 }
 
+/*
+Allow the user to look at the scores stored in the array. Iterate through
+the list printing each entry.
+*/
 func pastScore() {
 	if len(scores) == 0 {
 		fmt.Println("\nThere are currently no scores entered.")
@@ -36,23 +55,29 @@ func pastScore() {
 	}
 }
 
-func lowestToHighest() {
-
-}
-
+/*
+Get the average from the array of scores.
+*/
 func getMean() {
+	// Check to see if any scores are present.
 	if len(scores) == 0 {
 		fmt.Println("\nThere are currently no scores entered.")
 		return
 	}
+	// Get the sum of the scores
 	sum := 0
 	for _, score := range scores {
 		sum += score
 	}
+	// Calculate the mean
 	mean := float64(sum) / float64(len(scores))
 	fmt.Printf("Mean: %.2f\n", mean)
 }
 
+/*
+Runs the code. Gives the user an option to choose between what they want to
+do with the program.
+*/
 func main() {
 	for {
 		var input1 string
