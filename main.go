0b1b2c8dc1a1567b01553c1b9e4e0c13d1f7913e
Jack Holy <jholy@carroll.edu>
2024-03-06 20:45:11 -0700

    Create a truly working program

--- main.go
+++ main.go
@@ -3,68 +3,72 @@ package main
 import (
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 )
 
-type ScoreSheet struct {
-	scores       []int
-	numScores    int
-	highestScore int
-	lowestScore  int
-	mean         float32
-}
+var scores []int
 
-func newScore(sheet *ScoreSheet) string {
+func newScore() string {
 	for {
-		fmt.Println("\nEnter a new score: ")
+		fmt.Println("Enter a new score: ")
 		var score int
-		_, err := fmt.Scanln(&score)
+		_, err := fmt.Scan(&score)
 		if err != nil {
-			return "Invalid input. Please enter an integer."
+			fmt.Println("\nInvalid input. Please enter an integer.")
 		} else {
-			sheet.scores = append(sheet.scores, score)
-			pastScore(sheet)
-			return "Added " + strconv.Itoa(score) + " to list."
+			scores = append(scores, score)
+			// sort the list of scores
+			sort.Ints(scores)
+			return "Added " + strconv.Itoa(score) + " to list of scores."
 		}
 	}
 }
 
-func pastScore(sheet *ScoreSheet) {
-	fmt.Println("Past Scores: ")
-	for i, score := range sheet.scores {
-		fmt.Printf("%d: %d\n", i+1, score)
+func pastScore() {
+	if len(scores) == 0 {
+		fmt.Println("\nThere are currently no scores entered.")
+	} else {
+		fmt.Println("Past Scores: ")
+		for i, score := range scores {
+			fmt.Printf("%d: %d\n", i+1, score)
+		}
 	}
 }
 
-func getMean(scores []int) float64 {
-	sum := 0.0
-	for score := 0; score < len(scores); score++ {
-		sum += float64(score)
+func lowestToHighest() {
+
+}
+
+func getMean() {
+	if len(scores) == 0 {
+		fmt.Println("\nThere are currently no scores entered.")
+		return
 	}
-	mean := sum / float64(len(scores))
-	return mean
+	sum := 0
+	for _, score := range scores {
+		sum += score
+	}
+	mean := float64(sum) / float64(len(scores))
+	fmt.Printf("Mean: %.2f\n", mean)
 }
 
 func main() {
 	for {
-		// A blank sheet to store the input1 scores.
-		sheet1 := &ScoreSheet{
-			scores:       []int{},
-			numScores:    0,
-			highestScore: 0,
-			lowestScore:  0,
-			mean:         0}
 		var input1 string
-		fmt.Println("\nWelcome to your score keeper")
-		fmt.Println("Press [n] to enter a New score\n" +
-			"Press [p] to view past scores\n" +
-			"Press [q] to quit the program")
-		fmt.Scanln(&input1)
+		fmt.Println("\nScore Tracker:")
+		fmt.Println("Press [n] to enter a NEW score\n" +
+			"Press [p] to view PAST scores\n" +
+			"Press [m] to see the MEAN of the scores\n" +
+			"Press [q] to QUIT the program")
+		fmt.Scan(&input1)
 		switch input1 {
 		case "n":
-			fmt.Println(newScore(sheet1))
+			fmt.Println(newScore())
 		case "p":
-			pastScore(sheet1)
+			pastScore()
+		case "m":
+			getMean()
 		case "q":
 			fmt.Println("Quitting program...")
 			os.Exit(0)
